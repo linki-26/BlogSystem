7538b3bed53bf04d32a2f57a72134077de0cd8d3
linki-26 <liwben26@gmail.com>
2026-04-30 13:15:57 +0300

    modified gitignore file

--- google-config.js
+++
@@ -1,5 +0,0 @@
-// Google OAuth client configuration.
-// Replace this with the Web client ID from Google Cloud Console.
-window.GOOGLE_AUTH_CONFIG = {
-    clientId: '817207862333-ggsdunlkdlj3b42r82b7p00mhpmr34t9.apps.googleusercontent.com',
-};
