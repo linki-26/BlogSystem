974b68180427ad2a7cacf60e88df9719502e6648
linki-26 <liwben26@gmail.com>
2026-04-30 13:16:50 +0300

    added google example file

---
+++ google-config.example.js
@@ -0,0 +1,6 @@
+// Copy this file to google-config.js.
+// Paste your Google OAuth Web Client ID from Google Cloud Console.
+
+window.GOOGLE_AUTH_CONFIG = {
+    clientId: 'YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com',
+};
